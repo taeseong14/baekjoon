c0f4db9012da522c91b427b994173115562c4271
babpool <88077395+taeseong14@users.noreply.github.com>
2022-03-13 18:12:59 -0500

    Create 2845.cpp

---
+++ 2845.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int people = a * b;
+    int c, d, e, f, g;
+    cin >> c >> d >> e >> f >> g;
+    cout << c - people << " " << d - people << " " << e - people << " " << f - people << " " << g - people;
+}
